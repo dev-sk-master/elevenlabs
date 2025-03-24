77d23e50ea24f5f974417f1fa69e290d675fd783
dev_sk <sibu.kk@digitechsolutions.co.in>
2025-03-24 22:23:48 +0530

    chhhhhhhh

--- elevenlabs-server/netlify/functions/express.js
+++ elevenlabs-server/netlify/functions/express.js
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
 import app from '../../index.js';
 
-export const handler = serverless(app);
+export const handler = serverless(app, {
+  framework: 'express' // Explicitly specify the framework!
+});
