df523ebd7598b114c452ed7acfec31c1464c0a0b
dev_sk <sibu.kk@digitechsolutions.co.in>
2025-03-24 22:06:53 +0530

    asd

--- elevenlabs-server/netlify/functions/express.js
+++ elevenlabs-server/netlify/functions/express.js
@@ -1,7 +1,4 @@
-//import serverless from 'serverless-http';
+import serverless from 'serverless-http';
 import app from '../../index.js';
-import { Handler } from '@netlify/functions'; // native Netlify handler (preferred)
 
-
-
-export const handler = Handler(app);
+export const handler = serverless(app);
