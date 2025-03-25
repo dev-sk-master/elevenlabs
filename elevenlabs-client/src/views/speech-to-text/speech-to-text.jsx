a8c940d4598a507425ba05870e23b384057b0f45
dev_sk <sibu.kk@digitechsolutions.co.in>
2025-03-25 13:32:48 +0530

    chh

--- elevenlabs-client/src/views/speech-to-text/speech-to-text.jsx
+++ elevenlabs-client/src/views/speech-to-text/speech-to-text.jsx
@@ -196,7 +196,7 @@ const SpeechToText = () => {
   // }, []);
 
 
-  const [formData, setFormData] = useState({ language: "auto", silenceDuration: 2000 })
+  const [formData, setFormData] = useState({ language: "auto", silenceDuration: 1000 })
   const formDataRef = useRef(formData);
 
   const [isRecording, setIsRecording] = useState(false);
