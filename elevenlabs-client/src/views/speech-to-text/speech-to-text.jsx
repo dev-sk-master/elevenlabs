2573b1b8a5c5ac464c3b253c62a6272b20ee3c7a
dev-sk-master <sibu.kk@digitechsolutions.co.in>
2025-04-09 15:26:03 +0530

    changes

--- elevenlabs-client/src/views/speech-to-text/speech-to-text.jsx
+++ elevenlabs-client/src/views/speech-to-text/speech-to-text.jsx
@@ -430,14 +430,14 @@ const SpeechToText = () => {
     } catch (error) {
       console.error('Transcription error:', error);
 
-      setTranscriptions(prev =>
-        prev.map(item => (item.uuid === uuid ? {
-          ...item, text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+//       setTranscriptions(prev =>
+//         prev.map(item => (item.uuid === uuid ? {
+//           ...item, text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 
-`, status: 'completed'
-        } : item))
-      );
-      return;
+// `, status: 'completed'
+//         } : item))
+//       );
+//       return;
       //setTranscriptions(prev => [...prev, { error: error.message, audio: { url: audioUrl, mimeType } }]);
       // Step 2 (Error Case): Update transcription with error message
       setTranscriptions(prev =>
